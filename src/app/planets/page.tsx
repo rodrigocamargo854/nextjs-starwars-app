fc85c732832593802f12d4efeda02a0822c3e528
Rodrigo Camargo <47711649+rodrigocamargo854@users.noreply.github.com>
2025-10-22 08:01:19 -0300

    rota 1

---
+++ src/app/planets/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function PlanetsPage() {
+  return (
+    <main className="flex flex-col min-h-screen items-center p-8">
+      <h1 className="text-2xl font-bold mb-4">Star Wars Planets</h1>
+      <p>aqui</p>
+    </main>
+  );
+}
